4fd986aa859fbf601fa9a8bb1366fd7817ce2812
Parmeet Singh <parmeet.singh262@gmail.com>
2019-05-06 03:32:04 +0000

    stream camera updates

--- stream_camera.py
+++ stream_camera.py
@@ -1,12 +1,25 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture('rtsp://admin:admin@192.168.214.40/h264.sdp?res=half&x0=0&y0=0&x1=1920&y1=1080&qp=16&doublescan=0&ssn=41645')
+
+
+while(1):
+	cap = cv2.VideoCapture("http://192.168.2.54:8080/video")
+	print(cap)
+	if cap is None or not cap.isOpened():
+		print(cap)
+		print('Warning: unable to open video source: ')
+	else:
+		print("Camera is connected")
+		break	
+
 
 count = 0
 while(True):
     count = count + 1
     ret, frame = cap.read()
+    if cap is None or not cap.isOpened():
+    	print('Warning: unable to open video source: ', source)
     size = frame.shape
     cv2.imshow('frame',frame)
     #if count%30==0:
@@ -19,9 +32,9 @@ while(True):
                          [0, 0, 1]], dtype = "double"
                          )
     print(camera_matrix)
-    if count%20==0:
-        print("Capturing")
-        cv2.imwrite("captured_images/stream2"+str(count)+".jpg",frame)
+    #if count%20==0:
+    #    print("Capturing")
+        #cv2.imwrite("captured_images/stream2"+str(count)+".jpg",frame)
     #print(camera_matrix)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
